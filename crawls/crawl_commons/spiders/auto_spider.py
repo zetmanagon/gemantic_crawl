56c48d34863fa96101673e3822c970ed37cd97f5
zetmanagon <bibyh@qq.com>
2019-03-08 17:31:04 +0800

    重构

--- crawls/crawl_commons/spiders/auto_spider.py
+++ crawls/crawl_commons/spiders/auto_spider.py
@@ -475,8 +475,4 @@ class AutoSpider(scrapy.Spider,AbstractSpider):  # 需要继承scrapy.Spider类
 
 
     def closed(self,reason):
-        self.log("on close start stat seeds %s %s" % (self.crawlId,self.name))
-        self.log(reason)
-        if self.crawlId > 0:
-            self.seedDB.stat_seed(self.crawlId)
-        self.log("%s %s stat seeds finished" % (self.crawlId,self.name))
+        self.do_closed(reason)
