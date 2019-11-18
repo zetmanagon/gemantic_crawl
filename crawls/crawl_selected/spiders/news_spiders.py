e0438d470a7eb406264230b40d293fe741e0ec26
zetmanagon <bibyh@qq.com>
2019-11-18 10:56:49 +0800

    更新频率高的爬虫

---
+++ crawls/crawl_selected/spiders/news_spiders.py
@@ -0,0 +1,46 @@
+from crawl_commons.spiders.common_spider import *
+
+#单独测试使用
+class NewsSpider(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider1(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider1" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider2(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider2" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider3(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider3" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider4(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider4" # 定义蜘蛛名
+
+
+#单独测试使用
+class NewsSpider5(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider5" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider6(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider6" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider7(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider7" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider8(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider8" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider9(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider9" # 定义蜘蛛名
+
+#单独测试使用
+class NewsSpider10(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "news_spider10" # 定义蜘蛛名
