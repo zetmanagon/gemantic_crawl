22d96ee674b0101743104c52046ac3c0f4982f2d
zetmanagon <bibyh@qq.com>
2019-11-19 17:40:54 +0800

    爬虫新增节点

---
+++ crawls/crawl_selected/spiders/history_news_spiders.py
@@ -0,0 +1,46 @@
+from crawl_commons.spiders.common_spider import *
+
+#单独测试使用
+class HistoryNewsSpider(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider1(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider1" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider2(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider2" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider3(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider3" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider4(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider4" # 定义蜘蛛名
+
+
+#单独测试使用
+class HistoryNewsSpider5(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider5" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider6(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider6" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider7(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider7" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider8(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider8" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider9(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider9" # 定义蜘蛛名
+
+#单独测试使用
+class HistoryNewsSpider10(CommonSpider):  # 需要继承scrapy.Spider类
+   name= "history_news_spider10" # 定义蜘蛛名
